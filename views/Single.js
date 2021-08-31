a52ad752eb08069dfd4fcb095ecd12b00f104bf6
aleksikyto <aleksi.kyto@metropolia.com>
2021-08-31 08:51:48 +0300

    timeadded

--- views/Single.js
+++ views/Single.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {StyleSheet, SafeAreaView, Text, Image} from 'react-native';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
+import {format} from 'date-fns';
 
 const Single = ({route}) => {
   const {params} = route;
@@ -13,6 +14,7 @@ const Single = ({route}) => {
         source={{uri: uploadsUrl + params.filename}}
       />
       <Text>{params.description}</Text>
+      <Text>{format(new Date(params.time_added), 'dd.MM.yyyy')}</Text>
     </SafeAreaView>
   );
 };
